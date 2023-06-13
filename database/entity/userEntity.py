eebb1ffc7d85c931fbeb4f3913887ff1ca995bfb
Prasundas99 <prasundas115@gmail.com>
2023-06-13 14:54:36 +0530

    added timestamps

--- database/entity/userEntity.py
+++ database/entity/userEntity.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException
 from bson.objectid import ObjectId
 from passlib.hash import bcrypt
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 from database.collections import userEntity
 
@@ -10,16 +11,23 @@ class User(BaseModel):
     name: str
     email: EmailStr
     password: str
+    createdAt: datetime
+    updatedAt: datetime
 
     @staticmethod
     async def create_user(name: str, email: str, password: str):
         hashed_password = bcrypt.hash(password)
-        user_data = {"name": name, "email": email, "password": hashed_password}
-        print(user_data)
+        current_time = datetime.now()
+        user_data = {
+            "name": name,
+            "email": email,
+            "password": hashed_password,
+            "createdAt": current_time,
+            "updatedAt": current_time
+        }
         result = await userEntity.insert_one(user_data)
-        print(result)
         user_id = str(result.inserted_id)
-        return User(id=user_id, name=name, email=email, password=hashed_password)
+        return User(id=user_id, name=name, email=email, password=hashed_password, createdAt=current_time, updatedAt=current_time)
 
     @staticmethod
     async def delete_user(user_id: str):
@@ -32,7 +40,14 @@ class User(BaseModel):
         users = []
         async for document in cursor:
             user_id = str(document["_id"])
-            item = User(id=user_id, name=document["name"], email=document["email"], password=document["password"])
+            item = User(
+                id=user_id,
+                name=document["name"],
+                email=document["email"],
+                password=document["password"],
+                createdAt=document["createdAt"],
+                updatedAt=document["updatedAt"]
+            )
             users.append(item.dict())
         return users
 
@@ -41,11 +56,29 @@ class User(BaseModel):
         document = await userEntity.find_one({"_id": ObjectId(user_id)})
         if not document:
             raise HTTPException(status_code=404, detail="User not found")
-        return User(id=str(document["_id"]), name=document["name"], email=document["email"], password=document["password"])
+        return User(
+            id=str(document["_id"]),
+            name=document["name"],
+            email=document["email"],
+            password=document["password"],
+            createdAt=document["createdAt"],
+            updatedAt=document["updatedAt"]
+        )
 
     @staticmethod
     async def find_one_user_by_email(user_email: str):
-        return  await userEntity.find_one({"email": user_email})
+        return await userEntity.find_one({"email": user_email})
+    
+    @staticmethod
+    async def update_user(user_id: str, name: str, email: str):
+        current_time = datetime.now()
+        update_data = {"name": name, "email": email, "updatedAt": current_time}
+        result = await userEntity.update_one({"_id": ObjectId(user_id)}, {"$set": update_data})
+        if result.matched_count == 0:
+            raise HTTPException(status_code=404, detail="User not found")
+        return True
+
+
     # Create indexes on id and email fields
     async def create_indexes():
         await userEntity.create_index("id")
